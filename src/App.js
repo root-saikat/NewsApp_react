4b26438d11a794de3747f0d10630b95afb7200f6
Saikat <44300226+root-saikat@users.noreply.github.com>
2023-08-20 16:55:14 +0600

    hiding api key using env

--- src/App.js
+++ src/App.js
@@ -6,33 +6,32 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Footer from './Components/Footer';
 
 
-
 export default class App extends Component {
+
+  pageSize = 6;
+  apiKey = process.env.REACT_APP_API_KEY;
+  
   render() {
-
-    const pageSize = 6;
-    const apiKey = "97ecad9c95754c2ebb28927a8cf1947d"
-
     return (
       <div id='main-container'>
         <BrowserRouter>
             <Navbar navTitle='Top-News'/>
             <Routes>
-                  <Route exact path="/" element={<News apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}>
+                  <Route exact path="/" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/>}>
                   </Route> 
-                  <Route exact path="/business" element={<News apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/>}>
+                  <Route exact path="/business" element={<News apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"/>}>
                   </Route> 
-                  <Route exact path="/entertainment" element={<News apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}>
+                  <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}>
                   </Route> 
-                  <Route exact path="/general" element={<News apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}>
+                  <Route exact path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/>}>
                   </Route> 
-                  <Route exact path="/health" element={<News apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/>}>
+                  <Route exact path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"/>}>
                   </Route> 
-                  <Route exact path="/science" element={<News apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/>}>
+                  <Route exact path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"/>}>
                   </Route> 
-                  <Route exact path="/sports" element={<News apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/>}>
+                  <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"/>}>
                   </Route> 
-                  <Route exact path="/technology" element={<News apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}>
+                  <Route exact path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology"/>}>
                   </Route> 
             </Routes>
             <Footer/>
